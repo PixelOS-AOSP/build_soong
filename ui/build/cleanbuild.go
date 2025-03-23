fef17ffa29f5a8822128a1ff540d04de41eda857
koko-07870 <ko282ko82@gmail.com>
2025-03-23 20:11:36 +0530

    cleanbuild: add *.zip.md5sum to installclean_files
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- ui/build/cleanbuild.go
+++ ui/build/cleanbuild.go
@@ -126,6 +126,7 @@ func installClean(ctx Context, config Config) {
 		productOut("*.img"),
 		productOut("*.zip"),
 		productOut("*.zip.sha256sum"),
+                productOut("*.zip.md5sum"),
 		productOut("android-info.txt"),
 		productOut("misc_info.txt"),
 		productOut("apex"),
